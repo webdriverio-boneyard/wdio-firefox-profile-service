2302d9bde66624b24080b74d3e24b7c47cf19983
christian-bromann <mail@christian-bromann.com>
2017-03-23 11:07:06 +0100

    apply firefox profile to multiremote properly

---
+++ test/wdio.multiremote.conf.js
@@ -0,0 +1,40 @@
+var chai = require('chai')
+var path = require('path')
+
+exports.config = {
+    protocol: 'http',
+    port: 4445,
+    host: 'localhost',
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+
+    specs: [path.resolve(__dirname, 'test.multiremote.spec.js')],
+    capabilities: {
+        firefox: {
+            browserName: 'firefox',
+            platform: 'OS X 10.11',
+            version: '45.0',
+            name: 'wdio-firefox-profile-service',
+            build: 'wdio-firefox-profile-service - ' + process.env.TRAVIS_BUILD_NUMBER,
+            'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+        }
+    },
+    baseUrl: 'http://google.com',
+    framework: 'mocha',
+    logLevel: 'verbose',
+    reporters: ['dot'],
+    services: [
+        require('../launcher')
+    ],
+    firefoxProfile: {
+        extensions: [path.join(__dirname, '/fixtures/WebdriverIOTestExtension.xpi')],
+        'browser.startup.homepage': 'http://webdriver.io'
+    },
+    mochaOpts: {
+        timeout: 9999999,
+        ui: 'bdd'
+    },
+    before: function () {
+        chai.should()
+    }
+}
